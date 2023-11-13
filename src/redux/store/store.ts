6c7f51f5a4aee04d2af303f8f99c2b6ff82634a2
Yauhen Kalabukhin <coocooroozza@gmail.com>
2023-11-13 20:52:29 +0300

    feat: created store

---
+++ src/redux/store/store.ts
@@ -0,0 +1,31 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import modalReducer from '../slices/modal.slice';
+
+export const rootReducer = combineReducers({
+  modal: modalReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(),
+});
+
+export const persistor = persistStore(store);
+
+export default store;
+
+export type RootState = ReturnType<typeof rootReducer>
