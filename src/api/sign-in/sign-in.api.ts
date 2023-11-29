82453476a5d955bf12003828b2c4404a891b99c6
Yauhen Kalabukhin <coocooroozza@gmail.com>
2023-11-29 21:40:27 +0300

    feat: created sign up api

--- src/api/sign-in/sign-in.api.ts
+++ src/api/sign-in/sign-in.api.ts
@@ -1,12 +1,17 @@
 /* eslint-disable import/no-anonymous-default-export */
 import { default as axios } from 'axios';
-import { LoginBody, LoginResp } from '../../types/index';
+import { SignInResp, SignInUpBody, SignUpResp } from '../../types/index';
 import { apiCall } from '../../utils/index';
 
-const signIn = (data: LoginBody): Promise<LoginResp> => {
-  return apiCall<LoginResp>(axios.post('http://localhost:8000/api/auth/login', data));
+const signIn = (data: SignInUpBody): Promise<SignInResp> => {
+  return apiCall<SignInResp>(axios.post('http://localhost:8000/api/auth/login', data));
+};
+
+const signUp = (data: SignInUpBody): Promise<SignUpResp> => {
+  return apiCall<SignUpResp>(axios.post('http://localhost:8000/api/auth/register', data));
 };
 
 export default {
   signIn,
+  signUp,
 };
