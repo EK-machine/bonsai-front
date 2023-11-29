ba0725466bfa94d4e6ab18d29c3edc0037d46252
Yauhen Kalabukhin <coocooroozza@gmail.com>
2023-11-29 21:36:35 +0300

    feat: deleted unused files

--- src/app/broadmin/components/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './ModalPopup/modal-popup.component';
-export * from './SignIn/sign-in.component';
-export * from './SignModals/sign-in-modal.component';
-export * from './SignUp/sign-up.component';
-
-
-
--- src/components/svg/index.ts
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { Close } from './icons/close.component';
-
-export const Icon = {
-  Close,
-};
