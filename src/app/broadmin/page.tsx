5c404d30640b003f4c21d4fb53356fed8c7edffa
Yauhen Kalabukhin <coocooroozza@gmail.com>
2023-12-04 12:49:54 +0300

    feat: created initially BroAdmin page

--- src/app/broadmin/page.tsx
+++ src/app/broadmin/page.tsx
@@ -1,30 +1,14 @@
 'use client';
 
-import React from "react";
-import { useDispatch } from "react-redux";
-import { MODAL } from "../../consts/modal";
-import { openModal } from "../../redux/slices/modal.slice";
-import { EventTargetWithDataSetTagName } from "../../types/index";
-import { SignModals } from './components/index';
+import { AdminLayout } from '../components/index';
 import styles from './page.module.css';
 
 export default function BroAdmin() {
-    const dispatch = useDispatch()
-
-    const handleClick = (e: React.MouseEvent<HTMLElement>) => {
-        const eTerget = e.target as EventTargetWithDataSetTagName;
-        const {dataset: {openModalName}} = eTerget;
-        e.preventDefault();
-        dispatch(openModal(openModalName));
-    }
-
     return (
-        <div className={styles.broadmin_page}>
-            <section className={styles.broadmin_btns_wrapper}>
-                <button className={styles.broadmin_btn} onClick={handleClick} data-open-modal-name={MODAL.SIGN_IN}>Войти</button>
-                <button className={styles.broadmin_btn} onClick={handleClick} data-open-modal-name={MODAL.SIGN_UP}>Зарегистрироваться</button>
-            </section>
-            <SignModals />
+        <div className={styles.brologin_page}>
+            <AdminLayout>
+                <>here edit</>
+            </AdminLayout>
         </div>
     )
 }
