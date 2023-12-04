1a5ee7304f2ec309d1148d4f2258186c40319c18
Yauhen Kalabukhin <coocooroozza@gmail.com>
2023-12-04 12:33:00 +0300

    feat: added and modified signIn, logOut, refresh apis

--- src/api/sign-in/sign-in.api.ts
+++ src/api/sign-in/sign-in.api.ts
@@ -1,17 +1,37 @@
 /* eslint-disable import/no-anonymous-default-export */
 import { default as axios } from 'axios';
-import { SignInResp, SignInUpBody, SignUpResp } from '../../types/index';
+import { RefreshResp, SignInResp, SignInUpBody, SignUpResp } from '../../types/index';
 import { apiCall } from '../../utils/index';
 
 const signIn = (data: SignInUpBody): Promise<SignInResp> => {
-  return apiCall<SignInResp>(axios.post('http://localhost:8000/api/auth/login', data));
+  const config = {
+    withCredentials: true
+  };
+  return apiCall<SignInResp>(axios.post('http://localhost:8000/api/auth/login', data, config));
 };
 
 const signUp = (data: SignInUpBody): Promise<SignUpResp> => {
   return apiCall<SignUpResp>(axios.post('http://localhost:8000/api/auth/register', data));
 };
 
+const logOut = (token: string) => {
+  const config = {
+    headers: {'Authorization': `Bearer ${token}`},
+    withCredentials: true
+  };
+  return apiCall(axios.post('http://localhost:8000/api/auth/logout', {}, config));
+}
+
+const refresh = (): Promise<RefreshResp> => {
+  const config = {
+    withCredentials: true
+  };
+  return apiCall(axios.post('http://localhost:8000/api/auth/refresh', {}, config));
+}
+
 export default {
   signIn,
   signUp,
+  logOut,
+  refresh,
 };
